9d8295e237e1b16e2a63ceccabb05b062e4db7f5
Luka Šeremet <lseremet22@student.foi.hr>
2026-08-18 19:24:32 +0200

    Added ActiveReservationsRepository

--- RentacarioProject/ActiveReservations.cs
+++ RentacarioProject/ActiveReservations.cs
@@ -17,13 +17,7 @@ namespace RentacarioProject
         private int height;
         private int width;
         private Employee employee;
-      
-
-        private String connectionString =
-        "Server=31.147.206.65;" +
-        "Database=PI2526_lseremet22_DB;" +
-        "User Id=PI2526_lseremet22;" +
-        "Password=7*sW1R}.&7K)B-.);";
+        private ActiveReservationsRepository activeReservationsRepository;
 
         public ActiveReservations(Employee employee, int height, int width)
         {
@@ -32,80 +26,55 @@ namespace RentacarioProject
             this.height = height;
             this.width = width;
             userButton.Text = employee.getUsername();
+            this.activeReservationsRepository = new ActiveReservationsRepository();
         }
 
         private void AktivneRezervacije_Load(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            //sprema sve ucitane rezervacije u listu
+            List<Reservation> reservations = new List<Reservation>();
+            reservations = activeReservationsRepository.getActiveReservations();
+
+            foreach (Reservation r in reservations) 
             {
-                connection.Open();
-                string query = "select rezervacije.OIB, vrsterezervacija.naziv,"+
-                            " rezervacije.registracija, rezervacije.brojkilometara,"+
-                            " rezervacije.pocetak, rezervacije.kraj,"+
-                            " kupci.ime, kupci.prezime"+
-                            " from rezervacije join kupci"+
-                            " on rezervacije.OIB = kupci.OIB"+
-                            " join vrsterezervacija"+
-                            " on rezervacije.vrstarezervacije = vrsterezervacija.IDrezervacije"+
-                            " where sysdatetime()<= kraj"+
-                            " and sysdatetime()>= pocetak;" ;
+                Panel panel = new Panel();
+                panel.Size = new Size(400, 180);
+                panel.BorderStyle = BorderStyle.Fixed3D;
 
-                using (SqlCommand command = new SqlCommand(query, connection)) 
+                //kreiranje labele
+                Label label = new Label();
+                label.Text = "registracija: " + r.getRegistration().ToString() +
+                 "\nPocetak: " + r.getStart().ToString() +
+                 "\nKraj: " + r.getEnd().ToString();
+                label.Location = new Point(10, 10);
+                label.AutoSize = true;
+                label.Font = new Font("Arial", 10);
+                panel.Controls.Add(label);
+
+                //dodavanje slike
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Image = Image.FromFile("C:\\Users\\lukas\\source\\repos\\lseremet22\\ProgramskoInzenjerstvo\\RentacarioProject\\Images\\ibizaEdited.png");
+                pictureBox.Location = new Point(label.Width + 15, 15);
+                pictureBox.Size = new Size(200, 120);
+                panel.Controls.Add(pictureBox);
+
+                //dodavanje gumba
+                Button button = new Button();
+                button.Text = "Detalji rezervacije";
+                button.Name = "button" + r.getRegistration().ToString();
+                button.Location = new Point((panel.Width - button.Width) / 2, pictureBox.Bottom + 10);
+                button.AutoSize = true;
+
+                button.Click += (sender2, e2) =>
                 {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    { 
-                        while (reader.Read())
-                        {
-                            //dio koji se dogodi za svaki redak koji se procita iz baze
-                            Panel panel = new Panel();
-                            panel.Size = new Size(400, 180);
-                            panel.BorderStyle = BorderStyle.Fixed3D;
+                    //this.Hide();
+                    ReservationDetails reservationDetails = new ReservationDetails(width, height, r, employee);
+                    reservationDetails.Show();
+                };
 
-                            Label label = new Label();
-                            label.Text = "registracija: " + reader["registracija"].ToString() +
-                             "\nPocetak: " + reader["pocetak"].ToString() +
-                             "\nKraj: " + reader["kraj"].ToString();
-                            label.Location = new Point(10, 10);
-                            label.AutoSize = true;
-                            label.Font = new Font("Arial", 10);
-                            panel.Controls.Add(label);
+                panel.Controls.Add(button);
 
-                            //dodavanje slike
-                            PictureBox pictureBox = new PictureBox();
-                            pictureBox.Image = Image.FromFile("C:\\Users\\lukas\\source\\repos\\lseremet22\\ProgramskoInzenjerstvo\\RentacarioProject\\Images\\ibizaEdited.png");
-                            pictureBox.Location = new Point(label.Width + 15, 15);
-                            pictureBox.Size = new Size(200, 120);
-                            panel.Controls.Add(pictureBox);
-
-                            //dodavanje gumba
-                            Button button = new Button();
-                            button.Text = "Detalji rezervacije";
-                            button.Name = "button" + reader["registracija"].ToString();
-                            button.Location = new Point((panel.Width - button.Width) / 2, pictureBox.Bottom + 10);
-                            button.AutoSize = true;
-
-                            //kreiraj objekt reservation
-                            Reservation reservation = new Reservation(reader["OIB"].ToString(),reader["naziv"].ToString()
-                                ,reader["registracija"].ToString(), int.Parse(reader["brojkilometara"].ToString())
-                                , Convert.ToDateTime(reader["pocetak"]), Convert.ToDateTime(reader["kraj"]),
-                                reader["ime"].ToString(), reader["prezime"].ToString());
-
-                            button.Click += (sender2, e2) =>
-                            {
-                                //this.Hide();
-                                ReservationDetails reservationDetails = new ReservationDetails(width, height,reservation, employee);
-                                reservationDetails.Show();
-                            };
-
-                            panel.Controls.Add(button);
-
-                            flowLayoutPanel.Controls.Add(panel);
-
-                        }
-                        reader.Close();
-                    }
-
-                }
+                flowLayoutPanel.Controls.Add(panel);
             }
         }
 
---
+++ RentacarioProject/ActiveReservationsRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentacarioProject
+{
+public class ActiveReservationsRepository
+    {
+        private readonly string connectionString =
+            "Server=31.147.206.65;" +
+            "Database=PI2526_lseremet22_DB;" +
+            "User Id=PI2526_lseremet22;" +
+            "Password=7*sW1R}.&7K)B-.);";
+
+        public List<Reservation> getActiveReservations()
+        {
+            List<Reservation> reservations = new List<Reservation>();
+
+            string query =
+                "SELECT rezervacije.OIB, vrsterezervacija.naziv, " +
+                "rezervacije.registracija, rezervacije.brojkilometara, " +
+                "rezervacije.pocetak, rezervacije.kraj, " +
+                "kupci.ime, kupci.prezime " +
+                "FROM rezervacije " +
+                "JOIN kupci ON rezervacije.OIB = kupci.OIB " +
+                "JOIN vrsterezervacija " +
+                "ON rezervacije.vrstarezervacije = vrsterezervacija.IDrezervacije " +
+                "WHERE SYSDATETIME() <= kraj " +
+                "AND SYSDATETIME() >= pocetak;";
+
+            using (SqlConnection connection =
+                   new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command =
+                       new SqlCommand(query, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Reservation reservation = new Reservation(
+                            reader["OIB"].ToString(),
+                            reader["naziv"].ToString(),
+                            reader["registracija"].ToString(),
+                            int.Parse(reader["brojkilometara"].ToString()),
+                            Convert.ToDateTime(reader["pocetak"]),
+                            Convert.ToDateTime(reader["kraj"]),
+                            reader["ime"].ToString(),
+                            reader["prezime"].ToString()
+                        );
+
+                        reservations.Add(reservation);
+                    }
+                }
+            }
+
+            return reservations;
+        }
+    }
+}
